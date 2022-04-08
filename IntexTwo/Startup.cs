5d29f52bd520d46fd18d4e0660795bfc465debd4
kyliefromm <90372645+kyliefromm@users.noreply.github.com>
2022-04-07 19:34:48 -0600

    added csp header

--- IntexTwo/Startup.cs
+++ IntexTwo/Startup.cs
@@ -91,10 +91,46 @@ namespace IntexTwo
             app.UseAuthentication();
             app.UseAuthorization();
 
+            //app.Use(async (context, next) =>
+            //{
+            //    context.Response.Headers.Add("Content-Security-Policy-Report-Only", "default-src 'self'");
+            //    await next();
+            //});
 
+            // CSP
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("Content-Security-Policy-Report-Only", "default-src 'self'");
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+                context.Response.Headers.Add("Feature-Policy",
+                "vibrate 'self' ; " +
+                "camera 'self' ; " +
+                "microphone 'self' ; " +
+                "speaker 'self' https://youtube.com https://www.youtube.com ;" +
+                "geolocation 'self' ; " +
+                "gyroscope 'self' ; " +
+                "magnetometer 'self' ; " +
+                "midi 'self' ; " +
+                "sync-xhr 'self' ; " +
+                "push 'self' ; " +
+                "notifications 'self' ; " +
+                "fullscreen '*' ; " +
+                "payment 'self' ; ");
+
+                context.Response.Headers.Add(
+                "Content-Security-Policy-Report-Only",
+                "default-src 'self'; " +
+                "script-src-elem 'self'" +
+                "style-src-elem 'self'; " +
+                "img-src 'self'; http://www.w3.org/" +
+                "font-src 'self'" +
+                "media-src 'self'" +
+                "frame-src 'self'" +
+                "connect-src "
+
+                );
                 await next();
             });
 
